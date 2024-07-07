4774857abb2f2fd8f2ec7d9482f40379105ef07e
Gersom-laptop <gersomalaja@gmail.com>
2024-07-06 19:13:48 -0500

    [DETELE] postgresql and sequelize

--- server/src/config/dbConnect/index.js
+++ server/src/config/dbConnect/index.js
@@ -5,9 +5,6 @@ switch (DB_ENGINE) {
   case "mongodb":
     dbConnect = require("./mongodb")
     break
-  case "postgresql":
-    dbConnect = require("./postgresql")
-    break
   default:
     throw new Error("Environment variable 'DB_ENGINE' is not valid.")
 }
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,9 +5,6 @@ switch (DB_ENGINE) {
   case "mongodb":
     pathModel = 'mongoose'
     break
-  case "postgresql":
-    pathModel = 'sequelize'
-    break
   default:
     throw new Error("Environment variable 'DB_ENGINE' is not valid.")
 }
