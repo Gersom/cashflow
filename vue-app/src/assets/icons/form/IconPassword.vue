09f83d8f09b7f799b5f9c3f686bf7bef166702e7
Gersom-laptop <gersomalaja@gmail.com>
2024-07-15 23:49:17 -0500

    [ADD] icons and logoImage

---
+++ vue-app/src/assets/icons/form/IconPassword.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12.2 7.29999H2.4C1.6268 7.29999 1 7.92679 1 8.69999V13.6C1 14.3732 1.6268 15 2.4 15H12.2C12.9732 15 13.6 14.3732 13.6 13.6V8.69999C13.6 7.92679 12.9732 7.29999 12.2 7.29999Z" stroke="#9CA3AF" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M3.80008 7.3V4.5C3.80008 3.57174 4.16883 2.6815 4.82521 2.02513C5.48158 1.36875 6.37182 1 7.30008 1C8.22834 1 9.11858 1.36875 9.77495 2.02513C10.4313 2.6815 10.8001 3.57174 10.8001 4.5V7.3" stroke="#9CA3AF" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconPassword'
+}
+</script>
