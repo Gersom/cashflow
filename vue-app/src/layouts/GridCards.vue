19aaefc075e2af183965cc097dd1673a4b14a508
DaroOps <romerosd089@hotmail.com>
2024-08-04 17:33:48 -0500

    feat: :sparkles: Add more info to user store for better currency track

--- vue-app/src/layouts/GridCards.vue
+++ vue-app/src/layouts/GridCards.vue
@@ -7,19 +7,34 @@ defineOptions({
 
 <template>
   <div class="grid-cards">
-    <slot name="first"/>
-    <slot name="second"/>
-    <slot name="third"/>
+    <div class="card-item">
+      <slot name="first" />
+    </div>
+    <div class="card-item">
+      <slot name="second" />
+    </div>
+    <div class="card-item">
+      <slot name="third" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-    .grid-cards{
-        height: 80vh;
-        padding:0rem;
-        display: grid;
-        gap: 2rem;
-        /* grid-auto-rows:100%; */
-        grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
-    }
+.grid-cards {
+  height: 80vh;
+  padding: 0rem;
+  display: grid;
+  gap: 2rem;
+  /* grid-auto-rows:100%; */
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+}
+.card-item{
+  width: 100%;
+  height: 100%;
+  min-width: 360px;
+  min-height: 59px;
+  border-radius: var(--border-radius);
+  background-color: var(--background-color2);
+  padding: 25px;
+}
 </style>
---
+++ vue-app/src/pages/Home/components/HistoryCard.vue
@@ -0,0 +1,52 @@
+<script setup>
+import CardHead from '@components/CardHead/CardHead.vue'
+import TransactionBadge from '@components/TransactionBadge/TransactionBadge.vue';
+
+defineOptions({
+  name: 'HistoryCard'
+})
+</script>
+
+<template>
+  <div class="history-card">
+    <CardHead title="Movimientos de " :isSelect="true" />
+
+    <div class="movements-summary">
+      <div class="income-info">
+        <TransactionBadge :amount="1000.00" :isPositive="true" />
+        <p>Ingresos</p>
+      </div>
+      <div class="expense-info">
+        <TransactionBadge :amount="1000.00" :isPositive="false" />
+        <p>Gastos</p>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .history-card{   
+   
+    .movements-summary{
+      display: flex;
+      justify-content:space-between;
+      padding: 1em 0;
+      width: 70%;
+      & p{
+        margin: 0;
+        padding: 0;
+        color: var(--text-color);
+        line-height: 20px;
+      }
+    }
+
+
+  }
+</style>
+<!-- .card {
+  &.history-card {
+   
+  }
+} -->
