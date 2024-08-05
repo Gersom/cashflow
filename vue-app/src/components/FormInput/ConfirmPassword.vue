4f61ecaaefddb9e1aa35a7cf8ba3fe3150f0b8e7
Gersom-laptop <gersomalaja@gmail.com>
2024-08-05 02:27:10 -0500

    [ADD] inputs form

---
+++ vue-app/src/components/FormInput/ConfirmPassword.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from "vue";
+import InputPassword from "@components/CustomInput/InputPassword.vue";
+import IconPassword from "@icons/form/IconPassword.vue";
+import IconSuccess from "@icons/state/IconSuccess.vue";
+import IconWarning from "@icons/state/IconWarning.vue";
+
+const emit = defineEmits(["validate"]);
+const props = defineProps({
+  password: {
+    type: String,
+    default: "",
+  },
+});
+
+const inputValue = ref("");
+const stateValidation = ref(null);
+const text = ref("Confirmar contraseña");
+const textNotification = ref("");
+
+const validateValue = () => {
+  if (inputValue.value === "") {
+    textNotification.value = "";
+    stateValidation.value = null;
+    emit("validate", inputValue.value, false);
+  } else if (inputValue.value !== props.password) {
+    textNotification.value = "La contraseña no coincide";
+    stateValidation.value = false;
+    emit("validate", inputValue.value, false);
+  } else {
+    textNotification.value = "Contraseña coincide";
+    stateValidation.value = true;
+    emit("validate", inputValue.value, true);
+  }
+};
+</script>
+
+<template>
+  <div
+    class="user-name"
+    :class="{
+      'is-warning': stateValidation == false,
+      'is-success': stateValidation == true,
+    }"
+  >
+    <div class="title">
+      <div class="icon" v-if="stateValidation == true">
+        <IconSuccess />
+      </div>
+      <div class="icon" v-if="stateValidation == false">
+        <IconWarning />
+      </div>
+      <span>{{ textNotification || text }}</span>
+    </div>
+    <InputPassword
+      @input="validateValue"
+      v-model="inputValue"
+      :icon-component="IconPassword"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.user-name {
+  .title {
+    font-size: 14px;
+    color: var(--text-color);
+    margin: 0 0 5px 5px;
+    display: flex;
+  }
+  .icon {
+    height: 16px;
+    margin-right: 5px;
+  }
+
+  &.is-warning {
+    .title,
+    .icon {
+      color: var(--warning-color);
+    }
+  }
+  &.is-success {
+    .title,
+    .icon {
+      color: var(--success-color);
+    }
+  }
+}
+</style>
--- vue-app/src/stores/user.js
+++ vue-app/src/stores/user.js
@@ -13,8 +13,8 @@ export const useUserStore = defineStore('user', {
     currency: {
       amount: '9999.45',
       symbol: 'S/.',
-      name: 'Soles',
-      code: 'PEN',
+      name: 'sol peruano',
+      plural: 'soles',
       currencyPosition: 'before',
       decimalPlaces: 2
     }
@@ -29,3 +29,114 @@ export const useUserStore = defineStore('user', {
     }
   }
 })
+
+// [
+//   {
+//       "id": 1,
+//       "symbol": "B/.",
+//       "name": "balboa panameño",
+//       "plural": "balboas"
+//   },
+//   {
+//       "id": 2,
+//       "symbol": "Bs.",
+//       "name": "bolívar venezolano",
+//       "plural": "bolívares"
+//   },
+//   {
+//       "id": 3,
+//       "symbol": "Bs.",
+//       "name": "boliviano",
+//       "plural": "bolivianos"
+//   },
+//   {
+//       "id": 4,
+//       "symbol": "₡",
+//       "name": "colón costarricense",
+//       "plural": "colones"
+//   },
+//   {
+//       "id": 5,
+//       "symbol": "C$",
+//       "name": "córdoba nicaragüense",
+//       "plural": "córdobas"
+//   },
+//   {
+//       "id": 6,
+//       "symbol": "$",
+//       "name": "dólar",
+//       "plural": "dólares"
+//   },
+//   {
+//       "id": 7,
+//       "symbol": "$",
+//       "name": "dólar ecuatoriano",
+//       "plural": "dólares"
+//   },
+//   {
+//       "id": 8,
+//       "symbol": "€",
+//       "name": "euro",
+//       "plural": "euros"
+//   },
+//   {
+//       "id": 9,
+//       "symbol": "₲",
+//       "name": "guaraní paraguayo",
+//       "plural": "guaraníes"
+//   },
+//   {
+//       "id": 10,
+//       "symbol": "L",
+//       "name": "lempira hondureño",
+//       "plural": "lempiras"
+//   },
+//   {
+//       "id": 11,
+//       "symbol": "$",
+//       "name": "peso argentino",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 12,
+//       "symbol": "$",
+//       "name": "peso chileno",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 13,
+//       "symbol": "$",
+//       "name": "peso colombiano",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 14,
+//       "symbol": "$",
+//       "name": "peso dominicano",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 15,
+//       "symbol": "$",
+//       "name": "peso mexicano",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 16,
+//       "symbol": "$",
+//       "name": "peso uruguayo",
+//       "plural": "pesos"
+//   },
+//   {
+//       "id": 17,
+//       "symbol": "Q",
+//       "name": "quetzal guatemalteco",
+//       "plural": "quetzales"
+//   },
+//   {
+//       "id": 18,
+//       "symbol": "S/.",
+//       "name": "sol peruano",
+//       "plural": "soles"
+//   }
+// ]
