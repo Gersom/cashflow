2b62fefa3c4ef0485225614f4dd6f1098dac0589
DaroOps <romerosd089@hotmail.com>
2024-08-07 18:45:42 -0500

    style: :lipstick: Fix GridCards layout (sizing)

--- vue-app/src/layouts/GridCards.vue
+++ vue-app/src/layouts/GridCards.vue
@@ -21,18 +21,17 @@ defineOptions({
 
 <style scoped>
 .grid-cards {
-  height: 80vh;
-  padding: 0rem;
+  padding-bottom: 22px;
   display: grid;
-  gap: 2rem;
-  /* grid-auto-rows:100%; */
-  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  gap: 22px;
+  grid-auto-rows:591px;
+  grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
 }
 .card-item{
   width: 100%;
   height: 100%;
   min-width: 360px;
-  min-height: 59px;
+  overflow: hidden;
   border-radius: var(--border-radius);
   background-color: var(--background-color2);
   padding: 25px;
--- vue-app/src/pages/Home/components/CreateCard.vue
+++ vue-app/src/pages/Home/components/CreateCard.vue
@@ -16,67 +16,64 @@ defineOptions({
 const inputTextDefault = ref('')
 const inputCurrencyExpense = ref('0.00')
 const inputTextareaDefault = ref('')
-const inputRadioTwo  = ref({})
+const inputRadioTwo = ref({})
 const inputRadioDataTwo = [
-  { value: 'value1', text: 'Name 1' },
-  { value: 'value2', text: 'Name 2' }
+  { value: 'gasto', text: 'Gasto' },
+  { value: 'ingreso', text: 'Ingreso' }
 ]
 
-// Assuming inputRadioDataTwo is defined somewhere, you might want to add it here
-// const inputRadioDataTwo = [...]
 </script>
 
 <template>
   <div class="card create-card">
     <CardHead title="Crear nuevo movimiento" />
-    <div class="input-group">
-      <p>Titulo</p>
-      <CustomInputText v-model="inputTextDefault" />
+    <div class="form">
+      <div class="input-group">
+        <p>Titulo</p>
+        <CustomInputText v-model="inputTextDefault" />
+      </div>
+      <div class="input-group">
+        <p>Monto</p>
+        <CustomInputCurrency transaction-type="expense" v-model="inputCurrencyExpense" />
+      </div>
+      <div class="input-group">
+        <p>Tipo</p>
+        <CustomInputRadio :data="inputRadioDataTwo" v-model="inputRadioTwo" />
+      </div>
+      <div class="input-group">
+        <p>Descripción</p>
+        <CustomInputTextarea v-model="inputTextareaDefault" />
+      </div>
+      <div class="input-group">
+        <p>Categoria</p>
+        <InputCategory />
+      </div>
     </div>
-    <div class="input-group">
-      <p>Monto</p>
-      <CustomInputCurrency
-        transaction-type="expense"
-        v-model="inputCurrencyExpense"
-      />
-    </div>
-    <div class="input-group">
-      <p>Tipo</p>
-      <CustomInputRadio
-        :data="inputRadioDataTwo"
-        v-model="inputRadioTwo"
-      />
-    </div>
-    <div class="input-group">
-      <p>Descripción</p>
-      <CustomInputTextarea
-        v-model="inputTextareaDefault"
-      />
-    </div>
-    <div class="input-group">
-      <p>Categoria</p>
-      <InputCategory />
-    </div>
-    <CustomButton
-      :animation="true"
-      :icon-component="IconAdd"
-      text="Crear movimiento"
-    />
+    <CustomButton :animation="true" :icon-component="IconAdd" text="Crear movimiento" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .card {
   &.create-card {
+    & .form {
+      padding: 20px 0 20px 0;
+      display: flex;
+      flex-direction: column;
+      gap: 15px;
 
-    .input-group {
-      margin-bottom: 1rem;
-
-      p {
+      & .input-group>p {
+        margin: 0px;
+        padding: 0;
+        font-size: 14px;
+        font-weight: 400;
+        font-family: var(--font-nunito);
         margin-bottom: 0.5rem;
-        color: var(--text-color)
+        color: var(--subtitle-color);
+
       }
     }
+
   }
 }
 </style>
