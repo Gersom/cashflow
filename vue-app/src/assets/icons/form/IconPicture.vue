6adb06bfad8ed33e9e73e263461245c2d05c2d67
DaroOps <romerosd089@hotmail.com>
2024-08-09 18:59:02 -0500

    feat: :sparkles: Add profile image uploader card

--- vue-app/src/assets/icons/form/IconPicture.vue
+++ vue-app/src/assets/icons/form/IconPicture.vue
@@ -8,6 +8,6 @@
 
 <script>
 export default {
-  name: 'IconEyeSlash'
+  name: 'IconPicture'
 }
 </script>
--- vue-app/src/pages/Profile/components/ProfileCard.vue
+++ vue-app/src/pages/Profile/components/ProfileCard.vue
@@ -1,25 +1,90 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed} from 'vue'
 import CardTitle from '@components/CardTitle/CardTitle.vue'
 import CustomButton from "@components/CustomButton/index.vue"
-import IconAdd from "@icons/actions/IconAdd.vue"
-import CustomInputText from "@components/CustomInput/InputText.vue"
-import CustomInputCurrency from "@components/CustomInput/InputCurrency.vue"
-import CustomInputRadio from "@components/CustomInput/InputRadio.vue"
-import CustomInputTextarea from "@components/CustomInput/InputTextarea.vue"
-import InputCategory from "@components/CustomInput/InputCategory/InputCategory.vue"
+import IconSave from '@icons/actions/IconSave.vue';
+import IconPicture from '@icons/form/IconPicture.vue';
+import Url from '@components/FormInput/Url.vue';
+
+const urlValue = ref("");
+const urlValidate = ref(false);
+const handlerUrl = (text, isValid) => {
+  urlValue.value = text;
+  urlValidate.value = isValid;
+};
+
+const isButtonEnabled = computed(() => {
+  return urlValidate.value;
+});
 
 </script>
 
 <template>
   <div class="card card-profile">
-        <CardTitle title="Foto de perfil "/>
-  </div>
+    <CardTitle title="Foto de perfil "/>
+    <div class="card-content">
+
+        <div class="upload">
+            <p class="upload-title">
+              <IconPicture/> Sube tu imagen</p>
+            <CustomButton
+            :transparent="true"
+            text="Cargar desde ordenador"
+            />
+        </div>
+        <Url @validate="handlerUrl" />
+        <div class="flex-avatar">
+          <div class="user-avatar">
+            <img
+              src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="User profile image">
+          </div>
+        </div>
+        <CustomButton
+          :disabled="!isButtonEnabled"
+          :icon-component="IconSave"
+          text="Guardar imagen"
+        />
+        
+      </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 .card {
   height: 100%;
   width: 100%;
+  .card-content{
+
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    .upload{
+      display: flex;
+      flex-direction: column;
+      gap:15px;
+      .upload-title{
+        height: 14px;
+        color: var(--text-color);
+        font-size: 14px;
+        margin: 0;
+      }
+    }
+    .flex-avatar{
+      display: flex;
+      padding: 25px 0 25px 0;
+      justify-content: center;
+      .user-avatar {
+          position: relative;
+          width: 100px;
+          height: 100px;
+          overflow: hidden;
+          border-radius: 50%;
+          border: none;
+          background-color: var(--title-color);
+          box-shadow: 0px 0px 0px 0.5vh var(--title-color);
+        }
+    }
+  }
 }
 </style>
