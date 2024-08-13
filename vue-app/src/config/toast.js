f771f13156b364e35785fe56359e2db0086dc902
Gersom <gersomalaja@gmail.com>
2024-08-13 15:06:34 -0500

    [UPDATE] config env and [ADD] toast options

---
+++ vue-app/src/config/toast.js
@@ -0,0 +1,17 @@
+export const toastOptions = {
+  closeButton: "button",
+  closeOnClick: false,
+  draggable: false,
+  draggablePercent: 0.6,
+  hideProgressBar: true,
+  icon: true,
+  maxToasts: 20,
+  newestOnTop: true,
+  pauseOnFocusLoss: false,
+  pauseOnHover: true,
+  position: "top-right",
+  rtl: false,
+  showCloseButtonOnHover: false,
+  timeout: 5000,
+  transition: "Vue-Toastification__bounce",
+}
