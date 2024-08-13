57f2c8c8d581bfb848be66222644c934860fac12
Gersom <gersomalaja@gmail.com>
2024-08-13 15:14:38 -0500

    [UPDATE] config project main

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,14 +1,19 @@
-import './assets/styles/main.css';
+import './assets/styles/main.css'
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
-import VueApexCharts from 'vue3-apexcharts'
-import router from './router/index.js';
+import { toastOptions } from './config/toast.js'
 import App from './App.vue';
+import router from './router/index.js';
+import Toast from "vue-toastification"
+import VueApexCharts from 'vue3-apexcharts'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(Toast, toastOptions)
 app.use(VueApexCharts)
 app.use(pinia)
 app.use(router)
+
 app.mount('#app')
