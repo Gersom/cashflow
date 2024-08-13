6e679c41451a7f965ce81050124b9eba1b7d31d7
Gersom <gersomalaja@gmail.com>
2024-08-13 15:08:33 -0500

    [ADD] createRequest with axios

---
+++ vue-app/src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const createRequest = (method) => (options) => 
+  axios({
+    method,
+    withCredentials: true,
+    ...options
+  });
+
+export const apiPost = createRequest('POST');
+export const apiGet = createRequest('GET');
+export const apiPut = createRequest('PUT');
+export const apiDel = createRequest('DELETE');
