5da4bc77630de0cfde606dd5c8f8ae09780c29cb
DaroOps <romerosd089@hotmail.com>
2024-09-14 22:21:23 -0500

    fix: :ambulance: Access token became undef

--- node-server/src/api/auth/token/tokenController.js
+++ node-server/src/api/auth/token/tokenController.js
@@ -6,7 +6,8 @@ const { responseSuccess } = require("@utils/apiSuccess");
 const tokenController = {
   async refresh(req, res) {
     const { refresh_token } = req.cookies;
-    const { access_token } = await tokenService.refresh(refresh_token);
+    const  access_token  = await tokenService.refresh(refresh_token);
+    // console.log('access_token', access_token);
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: serv.nodeEnv === 'production',
@@ -23,7 +24,7 @@ const tokenController = {
   },
   async verify(req, res) {
     const { access_token } = req.cookies;
-    const user = await tokenService.verify(access_token);
+    await tokenService.verify(access_token);
     res.status(200).json(responseSuccess('Token verified successfully'));
   },
 };
