79edcdb9ac4041ba11af9f4f2a98022d60f7876f
Gersom <gersomalaja@gmail.com>
2024-09-15 21:58:37 -0500

    UPDATE CORS GENERATE

--- node-server/src/config/middlewares.js
+++ node-server/src/config/middlewares.js
@@ -7,8 +7,23 @@ const { serv } = require("@config/env.js");
 
 const middlewares = (app) => {
   // CORS configuration
+  const generateOrigin = () => {
+    try {
+      if (serv.isProduction) {
+        return JSON.parse(serv.allowedProdOrigins);
+      } else {
+        return JSON.parse(serv.allowedDevOrigins);
+      }
+    } catch (error) {
+      if (serv.isProduction) {
+        return serv.allowedProdOrigins;
+      } else {
+        return serv.allowedDevOrigins;
+      }
+    }
+  }
   const corsOptions = {
-    origin: serv.isProduction ? serv.allowedProdOrigins.split(',') : serv.allowedDevOrigins.split(','),
+    origin: generateOrigin(),
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
     credentials: true 
